85f1b4274fa35821fac01d13f1ebc90f9a6d93ce
JHOJAN DAVID AGUDELO BENJUMEA <BB7211@d03.abaigroup.local>
2023-11-17 16:24:07 -0500

    Segundo commit

--- Ejercicios Métodos/ejercicio2.js
+++ Ejercicios Métodos/ejercicio2.js
@@ -1,23 +1,61 @@
 const obtenerInformacion = (materia)=>{
     materias = {
-        fisica: ["pedro", "pepito", "cofla", "maria"],
-        programacion: ["pedro", "pepito"],
-        logica: ["pedro", "pepito", "cofla"],
-        quimica: ["pedro", "pepito", "cofla", "maria"],
+        fisica: ["Perez","pedro", "pepito", "cofla", "maria"],
+        programacion: ["Rodriguez","pedro", "pepito"],
+        logica: ["Dalto","pedro", "pepito", "cofla"],
+        quimica: ["Hernandez","pedro", "pepito", "cofla", "maria"]
         
     }
 
-    document.write(materias[materia] + "<br>");
-
     if (materias[materia] !== undefined) {
-        return materias[materia];
+        return [materias[materia],materia,materias];
     } else {
-        return false;
+        return materias;
     }
 }
 
-let informacion = obtenerInformacion("fisica");
+const mostrarInformacion = (materia)=>{
+    let informacion = obtenerInformacion(materia);
 
-if(informacion !== false){
-document.write(informacion);
+    if(informacion !== false){
+
+        let profesor = informacion[0][0];
+        let alumnos = informacion[0];
+        alumnos.shift();
+        // document.write(`Alumnos presentes en <b>${informacion[1]}</b>: <b> ${informacion[0]}</b><br>`);
+        document.write(`El profesor de <b>${informacion[1]}</b> es: <b style="color:red">${profesor}</b><br>
+        Los alumnos son: <b style="color:blue">${alumnos}</b><br><br>
+        
+        `);
+        }
 }
+
+const cantidadDeClases = (alumno)=>{
+    let informacion = obtenerInformacion();
+    let clasesPresentes = [];
+    let cantidadTotal = 0;
+    for (info in informacion){
+        if (informacion[info].includes(alumno)){
+            cantidadTotal++;
+            clasesPresentes.push(" "+ info);
+        }
+    }
+
+    return `<b style='color:blue'>
+    ${alumno}</b> está en <b>${cantidadTotal} clases: <b style='color:green'>${clasesPresentes}</b>
+    <br><br>
+    `;
+}
+
+
+mostrarInformacion("programacion");
+mostrarInformacion("fisica");
+mostrarInformacion("logica");
+mostrarInformacion("quimica");
+
+document.write(cantidadDeClases("cofla") + "<br>");
+document.write(cantidadDeClases("maria") + "<br>");
+document.write(cantidadDeClases("pedro") + "<br>");
+document.write(cantidadDeClases("pepito") + "<br>");
+
+
