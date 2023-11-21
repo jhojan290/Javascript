ae46714bf2bbfe722fbfc5d6bf68e0a0bc642d29
Jhojan David Agudelo Benjumea <117102269+jhojan290@users.noreply.github.com>
2023-11-21 00:36:54 -0500

    cuarto commit

--- Tipos de Inputs/main.js
+++ Tipos de Inputs/main.js
@@ -1,4 +1,6 @@
- const input = document.querySelector(".input-normal");
+//  const input = document.querySelector(".input-normal");
+
+const titulo = document.querySelector(".titulo");
 
 //  document.write(input.className); // accede a los atributos directamente desde javascript   
 
@@ -9,3 +11,18 @@
 // input.type = "color"; // para modificar estos objetos se puede realizar directamente desde el objeto, no hace falta utilizar setAttribute
 
 // input.accept = "image/jpg"; // para aceptar algún documento
+
+// input.minLength = 4;
+
+// input.setAttribute("minlength", 5); // cantidad minima de caracteres que tendrá el input
+
+// input.placeholder = "hola"; // lo que está dentro del input antes de editarlo
+
+// input.required = "required"; // Hace que sea obligatorio que un input sea llenado
+
+// titulo.style.color="#a22";
+
+titulo.style.backgroundColor = "#32b";
+
+// LOS MÉTODOS PARA DEFINIR, OBTENER Y ELIMINAR ATRIBUTOS SE USAN CON LA ETIQUETA ELEMENTS, MIENTRAS QUE LOS MÉTODOS DE SELECCIÓN DE ELEMENTOS,
+// SE USAN CON LA ETIQUETA DOCUMENT
